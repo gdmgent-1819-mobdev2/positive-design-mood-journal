528e169dc95eb70457084a7914f7c7615522967c
Hektor Misplon <hektor.misplon@gmail.com>
2019-02-14 21:05:23 +0100

    mood slider component

--- app/src/components/MoodSlider.js
+++ app/src/components/MoodSlider.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class MoodSlider extends Component {
   constructor(props) {
     super(props);
+    console.log(this.props);
     this.state = {
       value: 5,
       slideRange: 10,
