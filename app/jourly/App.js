0e585d941b36b6408653214bc92ab5c3dc516115
Hektor Misplon <hektor.misplon@gmail.com>
2019-03-13 11:47:14 +0100

    component refactoring

--- app/jourly/App.js
+++ app/jourly/App.js
@@ -2,61 +2,20 @@ import React from 'react';
 import Colors from './modules/Colors';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { LinearGradient } from 'expo';
+import Login from './views/Login';
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				{/* TODO: add image */}
-				<TextInput
-					style={styles.input}
-					placeholder="Email"
-					onChangeText={text => this.setState({ email })}
-				/>
-				<TextInput
-					style={styles.input}
-					placeholder="Password"
-					onChangeText={text => this.setState({ password })}
-				/>
-				<LinearGradient
-					style={styles.btn}
-					colors={[Colors.gradGreen1, Colors.gradGreen2]}
-					start={[0, 1]}
-					end={[1, 0]}
-				>
-					<Text style={styles.btnText}>Sign In</Text>
-				</LinearGradient>
-				{/* TODO: add Sign Up button */}
+				<Login />
 			</View>
 		);
 	}
 }
 
-const styles = StyleSheet.create({
-	btn: {
-		padding: 20,
-		flex: 1 / 24,
-		margin: 10,
-		borderRadius: 75,
-		justifyContent: 'center'
-	},
-	btnText: {
-		color: Colors.white
-	},
-	input: {
-		padding: 20,
-		flex: 1 / 24,
-		margin: 10,
-		flexDirection: 'row',
-		borderRadius: 75,
-		backgroundColor: Colors.lightGray
-	},
+styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		margin: 10,
-		flexDirection: 'column',
-		backgroundColor: '#fff',
-		alignItems: 'stretch',
-		justifyContent: 'center'
+		flex: 1
 	}
 });
---
+++ app/jourly/views/Login.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+
+import Colors from '../modules/Colors';
+import { LinearGradient } from 'expo';
+
+export default class Login extends Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				{/* TODO: add image */}
+				<TextInput
+					style={styles.input}
+					placeholder="Email"
+					onChangeText={text => this.setState({ email })}
+				/>
+				<TextInput
+					style={styles.input}
+					placeholder="Password"
+					onChangeText={text => this.setState({ password })}
+				/>
+				<LinearGradient
+					style={styles.btn}
+					colors={[Colors.gradGreen1, Colors.gradGreen2]}
+					start={[0, 1]}
+					end={[1, 0]}
+				>
+					<Text style={styles.btnText}>Sign In</Text>
+				</LinearGradient>
+				{/* TODO: add Sign Up button */}
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	btn: {
+		padding: 20,
+		flex: 1 / 24,
+		margin: 10,
+		borderRadius: 75,
+		justifyContent: 'center'
+	},
+	btnText: {
+		color: Colors.white
+	},
+	input: {
+		padding: 20,
+		flex: 1 / 24,
+		margin: 10,
+		flexDirection: 'row',
+		borderRadius: 75,
+		backgroundColor: Colors.lightGray
+	},
+	container: {
+		flex: 1,
+		margin: 10,
+		flexDirection: 'column',
+		backgroundColor: '#fff',
+		alignItems: 'stretch',
+		justifyContent: 'center'
+	}
+});
