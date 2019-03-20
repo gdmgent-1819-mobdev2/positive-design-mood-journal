d9478f94c1c94ada2e90350435cdfe3615268c2e
Hektor Misplon <hektor.misplon@gmail.com>
2019-03-20 13:11:55 +0100

    home refactoring

--- app/jourly/screens/Home.js
+++ app/jourly/screens/Home.js
@@ -1,37 +1,7 @@
 import React, { Component } from 'react';
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import { View, Text, Button, Image, StyleSheet } from 'react-native';
-
-// TODO: add bottom tab navigator
-class Home extends Component {
-	state = {};
-	render() {
-		return (
-			// TODO: display latest entry using component
-			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-				<Text>Home Screen</Text>
-				<Image
-					style={{ width: 50, height: 50 }}
-					source={{
-						uri:
-							'https://facebook.github.io/react-native/docs/assets/favicon.png'
-					}}
-				/>
-				<Button
-					title="Go back"
-					onPress={() => this.props.navigation.navigate('SignIn')}
-				/>
-				<Button
-					title="Settings"
-					onPress={() => this.props.navigation.navigate('Settings')}
-				/>
-			</View>
-=======
-=======
->>>>>>> Stashed changes
-
 import CurrentMood from '../components/CurrentMood';
+
 
 export default class Home extends Component {
 	state = {};
@@ -39,10 +9,6 @@ export default class Home extends Component {
 		return (
 			<CurrentMood />
 			// TODO: add bottom nav bar here
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 		);
 	}
 }
