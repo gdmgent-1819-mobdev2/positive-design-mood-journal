47bbc0b0cfb2375f2a4b3fe8b51382491e741442
Hektor Misplon <hektor.misplon@gmail.com>
2019-03-21 18:17:39 +0100

    reorganising

--- app/jourly/screens/JournalEntries.js
+++ app/jourly/screens/JournalEntries.js
@@ -1,86 +1,14 @@
 // view your journal entries
 //  searchbar
 //  latest entries
+import { View } from 'react-native';
 
 import React, { Component } from 'react';
-import {
-	KeyboardAvoidingView,
-	TextInput,
-	Button,
-	StyleSheet,
-	Picker,
-	TouchableOpacity,
-	Text,
-	View,
-	Header
-} from 'react-native';
-import Colors from '../modules/Colors';
 
-import CurrentMood from '../components/CurrentMood';
-import { bold } from 'ansi-colors';
-
-export default class JournalEntries extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			feelintTitle: '',
-			feelingText: ''
-		};
-	}
+export class JournalEntries extends Component {
 	render() {
-		return (
-			<KeyboardAvoidingView style={styles.container} behavior="padding">
-				{/* FIX ME here needs to come a header! */}
-				{/* input field for the title of your mood*/}
-				<TextInput
-					style={styles.title}
-					placeholder="Enter a title"
-					onChangeText={feelintTitle => this.setState({ feelintTitle })}
-				/>
-				{/* input field for the message of your mood*/}
-				<TextInput
-					style={styles.text}
-					placeholder="What's the matter?"
-					multiline={true}
-					onChangeText={feelingText => this.setState({ feelingText })}
-				/>
-				{/* button to save the message of the mood */}
-				{/* FIX ME: needs to be a round button with a checkmark*/}
-				<TouchableOpacity
-					onPress={() => this.props.navigation.navigate('Home')}
-				>
-					<Text style={{ textAlign: 'center' }}>Submit</Text>
-				</TouchableOpacity>
-			</KeyboardAvoidingView>
-		);
+		return <View />;
 	}
 }
 
-const styles = StyleSheet.create({
-	title: {
-		height: 60,
-		paddingLeft: 25,
-		margin: 10,
-		// FIXME: check if possible to set default font family globally?
-		fontFamily: 'fira-sans-light',
-		fontSize: 28,
-		fontWeight: 'bold'
-	},
-	text: {
-		flex: 0.55,
-		paddingLeft: 25,
-		margin: 10,
-		fontSize: 15,
-		// FIXME: check if possible to set default font family globally?
-		fontFamily: 'fira-sans-light'
-	},
-	container: {
-		flex: 1,
-		alignItems: 'stretch',
-		justifyContent: 'center',
-		flexDirection: 'column',
-		margin: 10,
-		backgroundColor: Colors.white
-	}
-});
+export default JournalEntries;
--- app/jourly/screens/JournalNoteEntry.js
+++ app/jourly/screens/JournalNoteEntry.js
@@ -2,16 +2,77 @@
 //  write a note for your journal entry
 //  add image button
 import React, { Component } from 'react';
-import NoteInput from '../components/NoteInput';
+import {
+	KeyboardAvoidingView,
+	TextInput,
+	StyleSheet,
+	TouchableOpacity,
+	Text
+} from 'react-native';
+import Colors from '../modules/Colors';
 
-export class JournalNoteEntry extends Component {
+export default class JournalNoteEntry extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			feelintTitle: '',
+			feelingText: ''
+		};
+	}
 	render() {
 		return (
-			// TODO: fullscreen textinput component
-			<NoteInput />
-			// TODO: display previous mood from slider on the side
+			<KeyboardAvoidingView style={styles.container} behavior="padding">
+				{/* FIX ME here needs to come a header! */}
+				{/* input field for the title of your mood*/}
+				<TextInput
+					style={styles.title}
+					placeholder="Enter a title"
+					onChangeText={feelintTitle => this.setState({ feelintTitle })}
+				/>
+				{/* input field for the message of your mood*/}
+				<TextInput
+					style={styles.text}
+					placeholder="What's the matter?"
+					multiline={true}
+					onChangeText={feelingText => this.setState({ feelingText })}
+				/>
+				{/* button to save the message of the mood */}
+				{/* FIX ME: needs to be a round button with a checkmark*/}
+				<TouchableOpacity
+					onPress={() => this.props.navigation.navigate('Home')}
+				>
+					<Text style={{ textAlign: 'center' }}>Submit</Text>
+				</TouchableOpacity>
+			</KeyboardAvoidingView>
 		);
 	}
 }
 
-export default JournalNoteEntry;
+const styles = StyleSheet.create({
+	title: {
+		height: 60,
+		paddingLeft: 25,
+		margin: 10,
+		// FIXME: check if possible to set default font family globally?
+		fontFamily: 'fira-sans-light',
+		fontSize: 28,
+		fontWeight: 'bold'
+	},
+	text: {
+		flex: 0.55,
+		paddingLeft: 25,
+		margin: 10,
+		fontSize: 15,
+		// FIXME: check if possible to set default font family globally?
+		fontFamily: 'fira-sans-light'
+	},
+	container: {
+		flex: 1,
+		alignItems: 'stretch',
+		justifyContent: 'center',
+		flexDirection: 'column',
+		margin: 10,
+		backgroundColor: Colors.white
+	}
+});
