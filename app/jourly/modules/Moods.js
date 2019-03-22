b0ac557cca5779feb46cd7c9f933032cbbe21d01
Hektor Misplon <hektor.misplon@gmail.com>
2019-03-22 09:09:55 +0100

    moods

---
+++ app/jourly/modules/Moods.js
@@ -0,0 +1,13 @@
+export default [
+    0 = 'Really bad',
+    1 = 'Quite bad',
+    2 = 'Bad',
+    3 = 'Meh',
+    4 = 'Not sure',
+    5 = 'Okay',
+    6 = 'Fine',
+    7 = 'Allright',
+    8 = 'Good',
+    9 = 'Great',
+    10 = 'Really great'
+];
